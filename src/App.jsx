6e2d81d1cc679476333367fa5d465b6cddd9a08d
frbarbre <fr.barbre@gmail.com>
2023-03-22 13:10:45 +0100

    sanity check

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Frederik Overgaard Barbre</h1>
+      <h1>Lean er vild med Java</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
